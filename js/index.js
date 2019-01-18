02cdeb57209beaa575446cb6ece1e8e8cf8cd940
João Paulo de Melo <jpmdik@gmail.com>
2019-01-18 01:09:03 +0000

    Att

--- js/index.js
+++ js/index.js
@@ -19,22 +19,37 @@ $( document ).ready(function() {
     $("#corpo").addClass("d-none");
     $("#loading").removeClass("d-none");
     
-    dados = Object();
-    dados.username = 'ola';
-    dados.voucher = '123'
-
-    var result = "";
+    dados = new Object();
+    dados.username = $("#user").val();
+    dados.voucher = $("#code").val();
 
     $.ajax({
       url: URL_API + "/confirmar",
       method: 'POST',
+      data: JSON.stringify(dados),
       dataType: 'json',
-      data: dados,
-      async: false,
+      contentType: 'application/json;charset=UTF-8',
       crossDomain: true,
       success: function(data, status, xhr){
         result = data;
-        alert(result);
+        if(result.success == false){
+          $("#corpo").removeClass("d-none");
+          $("#loading").addClass("d-none");
+          $("#alert").html(result.msg);
+          $("#alert").removeClass("d-none");
+        }
+        else if(result.success == true){
+          $("#frase").html("\""+result.msg.frase+"\"");
+          $("#autor").html("<i><b>"+result.msg.autor+"</b></i>");
+          $("#descricao").html(result.msg.descricao);
+          $("#data-missa").html("<b>Data: </b>"+result.msg['data-missa']);
+          $("#local-missa").html("<b>Local: </b>"+result.msg['local-missa']);
+          $("#data-formatura").html("<b>Data: </b>"+result.msg['data-formatura']);
+          $("#local-formatura").html("<b>Local: </b>"+result.msg['local-formatura']);
+          $("#loading").addClass("d-none");
+          $("#confirmacao").removeClass("d-none");
+        }
+        console.log(result);
       },
       error: function (request, status, error) {
         result = "";
