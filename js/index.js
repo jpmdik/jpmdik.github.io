c5d89986acc062623e8ee2771e9526e7e89ec069
random <service_random@outlook.com>
2019-01-17 22:48:32 -0200

    Att

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
+  $('#user').keyup(function(e){
+    if(e.keyCode == 13)
+      {
+          $("#code").focus();
+      }
+  });
+
+  $('#code').keyup(function(e){
+    if(e.keyCode == 13)
+      {
+          $("#confirmar").trigger("click");
+      }
+  });
+
   $("#confirmar").click(function() {
     $("#corpo").addClass("d-none");
     $("#loading").removeClass("d-none");
