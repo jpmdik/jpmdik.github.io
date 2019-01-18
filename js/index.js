3b00025a85ce13aedaed96b0ccedc488e99900ce
random <service_random@outlook.com>
2019-01-18 00:36:26 -0200

    send info

--- js/index.js
+++ js/index.js
@@ -1,20 +1,44 @@
+URL_API = "https://convite-form.herokuapp.com"
+
 $( document ).ready(function() {
   $('#user').keyup(function(e){
     if(e.keyCode == 13)
-      {
-          $("#code").focus();
-      }
+    {
+      $("#code").focus();
+    }
   });
-
+  
   $('#code').keyup(function(e){
     if(e.keyCode == 13)
-      {
-          $("#confirmar").trigger("click");
-      }
+    {
+      $("#confirmar").trigger("click");
+    }
   });
-
+  
   $("#confirmar").click(function() {
     $("#corpo").addClass("d-none");
     $("#loading").removeClass("d-none");
+    
+    dados = Object();
+    dados.username = 'ola';
+    dados.voucher = '123'
+
+    var result = "";
+
+    $.ajax({
+      url: URL_API + "/confirmar",
+      method: 'POST',
+      dataType: 'json',
+      data: dados,
+      async: false,
+      crossDomain: true,
+      success: function(data, status, xhr){
+        result = data;
+        alert(result);
+      },
+      error: function (request, status, error) {
+        result = "";
+      }
+    });
   });
 });
